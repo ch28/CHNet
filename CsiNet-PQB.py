cdce755f548f6e88cf73a829abf0ac490f4222e3
ch28 <94219590+ch28@users.noreply.github.com>
2022-04-10 16:28:41 +0800

    Update CsiNet-PQB.py

--- CsiNet-PQB.py
+++ CsiNet-PQB.py
@@ -1,3 +1,11 @@
+#-----------------------------------------------------------------------------------+
+# Author: Xin Liang                                                                 |
+# Time Stamp: Jan 4, 2021                                                           |
+# Affiliation: Beijing University of Posts and Telecommunications                   |
+# Email: liangxin@bupt.edu.cn                                                       |
+#-----------------------------------------------------------------------------------+
+#                             *** Open Source Code ***                              |
+#-----------------------------------------------------------------------------------+
 import tensorflow as tf
 from keras.layers import Input, Dense, BatchNormalization, Reshape, Conv2D, add, LeakyReLU, Activation, Lambda, multiply, Layer
 from keras.models import Model
